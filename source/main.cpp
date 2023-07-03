2e3961e0f695fdbab35ec61d0f5fd25d600d670a
Aron <luiggi33@web.de>
2023-07-03 13:37:53 +0200

    create function

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,44 @@
 ﻿#include <GarrysMod/Lua/Interface.h>
 
+#include <GarrysMod/InterfacePointers.hpp>
+
+#include <iserver.h>
+
+static IServer* server = nullptr;
+
+LUA_FUNCTION( ReconnectPlayer )
+{
+	LUA->CheckType( 1, GarrysMod::Lua::Type::Number );
+
+	int entIndex = (LUA->GetNumber( 1 ));
+
+	IClient* player = server->GetClient( entIndex );
+
+	if( player == nullptr ) {
+		LUA->ThrowError("invalid client given");
+	}
+
+	if (!player->IsFakeClient()) {
+		player->Reconnect();
+		LUA->PushBool( true );
+	} else {
+		LUA->PushBool( false );
+	}
+
+	return 1;
+}
+
 GMOD_MODULE_OPEN()
 {
+	server = InterfacePointers::Server();
+	if (server == nullptr) {
+		LUA->ThrowError("failed to dereference IServer");
+	} else {
+		LUA->PushSpecial( GarrysMod::Lua::SPECIAL_GLOB );
+			LUA->PushCFunction( ReconnectPlayer );
+			LUA->SetField( -2, "ReconnectPlayer" );
+		LUA->Pop();
+	}
 
 	return 0;
 }
