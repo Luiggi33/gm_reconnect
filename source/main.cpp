0ab35c10588e6565dd64dc5e235ea6be75284de2
Aron <luiggi33@web.de>
2023-07-14 00:23:27 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,7 @@
-﻿#include <GarrysMod/Lua/Interface.h>
+#include <GarrysMod/Lua/Interface.h>
 #include <GarrysMod/InterfacePointers.hpp>
-#include <iclient.h>
 #include <iserver.h>
+#include <iclient.h>
 
 static IServer* server = nullptr;
 
@@ -16,23 +16,25 @@ void LuaPrint(GarrysMod::Lua::ILuaBase* LUA, char* msg)
 
 LUA_FUNCTION( ReconnectPlayer )
 {
-	LuaPrint(LUA, "ReconnectPlayer called");
+	LUA->CheckType(1, GarrysMod::Lua::Type::Entity);
 
-	LUA->CheckType( 1, GarrysMod::Lua::Type::Number );
-	int entIndex = (LUA->GetNumber( 1 ));
-
-	IClient* player = server->GetClient( entIndex );
+	IClient* player = server->GetClient(LUA->GetNumber(1));
 
 	if (player == nullptr) {
-		LUA->ThrowError("Invalid client given");
+		LUA->ThrowError("Failed to dereference player");
 		return 0;
 	}
 
-	if (!player->IsFakeClient()) {
+	if (server == nullptr) {
+		LUA->ThrowError("Failed to dereference IServer");
+		return 0;
+	}
+
+	if (player->IsFakeClient()) {
+		LUA->PushBool(false);
+	} else {
 		player->Reconnect();
 		LUA->PushBool(true);
-	} else {
-		LUA->PushBool(false);
 	}
 
 	return 1;
@@ -51,6 +53,15 @@ GMOD_MODULE_OPEN()
 	LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
 		LUA->PushCFunction(ReconnectPlayer);
 		LUA->SetField(-2, "ReconnectPlayer");
+	LUA->Pop();
+
+	LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+		LUA->GetField(-1, "FindMetaTable");
+		LUA->PushString("Player");
+		LUA->Call(1, 1);
+		LUA->PushString("Reconnect");
+		LUA->PushCFunction(ReconnectPlayer);
+		LUA->SetTable(-3);
 	LUA->Pop();
 
 	return 0;
